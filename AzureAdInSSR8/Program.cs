b76ffa3701ff6896f12cdd6ed9e09e75dc7138a0
CodingArc4 <kmunib10@outlook.com>
2024-01-30 16:03:14 +0500

    fixed some errors

--- AzureAdInSSR8/Program.cs
+++ AzureAdInSSR8/Program.cs
@@ -1,5 +1,6 @@
 using AzureAdInSSR8.Components;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 
@@ -11,21 +12,42 @@ namespace AzureAdInSSR8
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var initialScopes = builder.Configuration["DownstreamApi:Scopes"]?.Split(' ') ?? builder.Configuration["MicrosoftGraph:Scopes"]?.Split(' ');
+
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
             // Add services to the container.
             builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"));
+               .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+               .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+               .AddMicrosoftGraph(builder.Configuration.GetSection("MicrosoftGraph"))
+               .AddInMemoryTokenCaches();
+
             builder.Services.AddControllersWithViews()
                 .AddMicrosoftIdentityUI();
+
+            builder.Services.AddRazorPages();
 
             builder.Services.AddAuthorization(options =>
             {
                 // By default, all incoming requests will be authorized according to the default policy
                 options.FallbackPolicy = options.DefaultPolicy;
             });
+
+
+            builder.Services.Configure<OpenIdConnectOptions>(OpenIdConnectDefaults.AuthenticationScheme, options =>
+            {
+                options.Events.OnSignedOutCallbackRedirect = async context =>
+                {
+                    context.HttpContext.Response.Redirect(context.Options.SignedOutRedirectUri);
+                    context.HandleResponse();
+                };
+
+            });
+
+            builder.Services.AddScoped<MicrosoftIdentityConsentAndConditionalAccessHandler>();
 
             var app = builder.Build();
 
@@ -46,14 +68,23 @@ namespace AzureAdInSSR8
             app.UseStaticFiles();
             app.UseAntiforgery();
 
-
+            app.MapRazorPages();
             app.MapControllers();
-            app.MapBlazorHub();
+            //app.MapBlazorHub();
 
             //app.MapFallbackToPage("/{param?}", "/_Host");
 
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
+
+            //app.UseRewriter(new RewriteOptions().Add(
+            //context =>
+            //{
+            //    if (context.HttpContext.Request.Path == "/MicrosoftIdentity/Account/SignedOut")
+            //    {
+            //        context.HttpContext.Response.Redirect("/");
+            //    }
+            //}));
 
             app.Run();
         }
